b27c0db2d4521af5cfc4645a45aaedb1dc3effd2
오의석 <dkdlelxoa@naver.com>
2023-04-18 16:36:05 +0900

    :hammer: chore init project #S08P31A304-134

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/XyzApplication.java
@@ -0,0 +1,13 @@
+package com.grouproom.xyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzApplication.class, args);
+	}
+
+}
---
+++ backend/xyz/src/test/java/com/grouproom/xyz/XyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouproom.xyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
