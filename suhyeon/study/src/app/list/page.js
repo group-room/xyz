6a90ad99db435f2dc8065d16263f1c0e98a5fdd8
송수현 <ssh1692@naver.com>
2023-04-19 17:45:41 +0900

    ✨ feat : Day0419 Next.js 학습

--- suhyeon/study/src/app/list/page.js
+++ suhyeon/study/src/app/list/page.js
@@ -1,13 +1,16 @@
 export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <div>
-      <h2>Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((item, idx) => {
+        return (
+          <div className="food" key={idx}>
+            <h4>{item} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
