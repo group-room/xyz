0dd0baf6f7aafb976b1e0e5a3ea8fa056d698be8
오의석 <dkdlelxoa@naver.com>
2023-04-20 11:07:08 +0900

    :sparkles: feat: 공통 response & error response 추가

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/global/model/BaseResponse.java
@@ -0,0 +1,36 @@
+package com.grouproom.xyz.global.model;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * packageName    : com.grouproom.xyz.global.model
+ * fileName       : BaseResponse
+ * author         : SSAFY
+ * date           : 2023-04-20
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * <p>
+ * 2023-04-20        SSAFY       최초 생성
+ */
+@Getter
+public class BaseResponse<T> {
+    private HttpStatus statusCode;
+    private String message;
+    private T data;
+
+    BaseResponse(T body) {
+        this.statusCode = HttpStatus.OK;
+        this.message = "성공";
+        this.data = body;
+    }
+
+    BaseResponse(HttpStatus statusCode, String message, T body) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = body;
+    }
+}
