2921339a6a92365c8fe84e226b94487ba5d62dcb
yujj25 <yujj25@naver.com>
2023-04-20 10:06:02 +0900

    :sparkles: feat : GroupMember Entity 생성

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/group/entity/GroupMember.java
@@ -0,0 +1,37 @@
+package com.grouproom.xyz.domain.group.entity;
+
+import com.grouproom.xyz.domain.album.entity.Album;
+import com.grouproom.xyz.domain.album.entity.UserAlbumID;
+import com.grouproom.xyz.domain.user.entity.User;
+import com.grouproom.xyz.global.model.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Entity
+@IdClass(UserGroupId.class)
+@Table(name = "group_memeber")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupMember extends BaseTimeEntity {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_sequence")
+    private User user;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_sequence")
+    private Group group;
+
+    @Column(name = "joined_at")
+    @Temporal(TemporalType.DATE)
+    private Date joinedAt;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
+    private Boolean isDeleated;
+}
