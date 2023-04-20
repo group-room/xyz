a53338ba0e5bd00d87d8a070f2d100f7eb2bac42
오의석 <dkdlelxoa@naver.com>
2023-04-20 10:24:32 +0900

    :sparkles: feat: 친구&차단 테이블 생성 #S08P31A304-140

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/entity/Friend.java
@@ -0,0 +1,49 @@
+package com.grouproom.xyz.domain.friend.entity;
+
+import com.grouproom.xyz.domain.user.entity.User;
+import com.grouproom.xyz.global.model.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * packageName    : com.grouproom.xyz.domain.friend.entity
+ * fileName       : Friend
+ * author         : SSAFY
+ * date           : 2023-04-20
+ * description    :
+ * <p>
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * <p>
+ * 2023-04-20        SSAFY       최초 생성
+ */
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@IdClass(FriendID.class)
+public class Friend extends BaseTimeEntity {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "from_user")
+    private User fromUser;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "to_user")
+    private User toUser;
+
+    @Column(name = "is_accepted", columnDefinition = "tinyint(1) default 0")
+    private Boolean isAccepted;
+
+    @Column(name = "chat_seq")
+    private Long chatSeq;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
+    private Boolean isDeleted;
+
+}
