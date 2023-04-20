48dedb1db4989ce9e6228fb76bbf18e22e04ef48
yujj25 <yujj25@naver.com>
2023-04-20 10:46:25 +0900

    :sparkles: feat : Notification Entity 생성

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/entity/Notification.java
@@ -0,0 +1,40 @@
+package com.grouproom.xyz.domain.notification.entity;
+
+import com.grouproom.xyz.domain.user.entity.User;
+import com.grouproom.xyz.global.model.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notification extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sequence")
+    private Long sequence;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_sequence")
+    private User user;
+
+    @Enumerated
+    @Column(name = "notification_type")
+    private NotificationType notificationType;
+
+    @Column(name = "target_sequence")
+    private Long targetSequence;
+
+    @Column(length = 200, name = "content")
+    private String content;
+
+    @Column(name = "is_received", columnDefinition = "tinyint(1) default 0")
+    private boolean ieReceived;
+
+    @Column(name = "is_deleted", columnDefinition = "tinyint(1) default 0")
+    private Boolean isDeleated;
+}
