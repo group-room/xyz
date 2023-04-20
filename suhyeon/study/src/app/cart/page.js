d9a7d50792fbded1cf07498cad88826bae33c9c8
송수현 <ssh1692@naver.com>
2023-04-20 17:37:12 +0900

    ✨feat: Day0420 Next.js Study

--- suhyeon/study/src/app/cart/page.js
+++ suhyeon/study/src/app/cart/page.js
@@ -1,7 +1,20 @@
 export default function Cart() {
+  let 장바구니 = ["Tomatoes", "Pasta"];
   return (
     <div>
-      <h1>장바구니입니다.</h1>
+      <h4>Cart</h4>
+      <CartItem 상품={장바구니[0]} />
+      <CartItem 상품={장바구니[1]} />
+    </div>
+  );
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.상품}</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
--- suhyeon/study/src/app/list/page.js
+++ suhyeon/study/src/app/list/page.js
@@ -1,15 +1,26 @@
-export default function List() {
-  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
+"use client";
 
+import { useState } from "react";
+
+export default function List() {
+  let list = ["Tomatoes", "Pasta", "Coconut"];
+  let [cnt, setCnt] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
-      {상품.map((item, idx) => {
-        return (
-          <div className="food" key={idx}>
-            <h4>{item} $40</h4>
-          </div>
-        );
+      {list.map((item, idx) => {
+        <div className="food" key={idx}>
+          {/* <img src={`/food${idx}.png`} className="food-img" /> */}
+          <h4>{item} $40</h4>
+          <span> {cnt[idx]} </span>
+          <button
+            onClick={() => {
+              setCnt(cnt[idx] + 1);
+            }}
+          >
+            +
+          </button>
+        </div>;
       })}
     </div>
   );
