01d9ccab0324e4ca207ec249651663b2069cc2ec
오의석 <dkdlelxoa@naver.com>
2023-04-21 17:43:23 +0900

    :bug: fix: user repository impl 오류 수정 (#S08P31A304-145)

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/user/repository/UserRepository.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/user/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.grouproom.xyz.domain.user.repository;
 
 import com.grouproom.xyz.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 }
