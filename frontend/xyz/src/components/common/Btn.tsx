31894ff01376e2bdf4345b3aaa5c4a8164e313b4
김경림 <myway01212@naver.com>
2023-04-24 14:31:24 +0900

    ✨ feat: 공통 컨테이너 컴포넌트 구현 (#S08P31A304-201)

--- frontend/xyz/src/components/common/Btn.tsx
+++ frontend/xyz/src/components/common/Btn.tsx
@@ -1,31 +1,25 @@
+import { BgColors } from "@/constants/style";
 import React from "react";
 
 type BtnProps = {
   bgColor: string;
   text: string;
-  func: () => void;
+  btnFunc: () => void;
 };
 
 /**
  *
  * @param bgColor 배경색
  * @param text 버튼 텍스트
- * @param func 클릭시 실행할 함수
+ * @param btnFunc 클릭시 실행할 함수
  */
 
-function Btn({ bgColor, text, func }: BtnProps) {
-  const colors = {
-    blue: "bg-blue",
-    pink: "bg-pink",
-    yellow: "bg-yellow",
-  };
-
+function Btn({ bgColor, text, btnFunc }: BtnProps) {
   return (
     <button
       type="button"
-      onClick={func}
-      className={`w-100 ${colors[bgColor]} rounded px-3 py-1 drop-shadow-md`}
-      style={{ border: "1px solid black" }}
+      onClick={btnFunc}
+      className={`w-100 ${BgColors[bgColor]} rounded px-3 py-1 drop-shadow-md border border-black`}
     >
       {text}
     </button>
---
+++ frontend/xyz/src/constants/style.tsx
@@ -0,0 +1,5 @@
+export const BgColors = {
+  blue: "bg-blue",
+  pink: "bg-pink",
+  yellow: "bg-yellow",
+};
