4556b436920a29a1b1d3d1edb55942b5de48d8f2
송수현 <ssh1692@naver.com>
2023-04-24 17:42:58 +0900

    💄 style: 공통컴포넌트 프로필이미지 디자인 #148

--- frontend/xyz/src/app/capsule/page.tsx
+++ frontend/xyz/src/app/capsule/page.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import SearchBar from './../../components/searchbar/SearchBar'
+import ProfileImgRegister from './../../components/profileImg/ProfileImgRegister'
+import ProfileImg from './../../components/profileImg/ProfileImg'
+
 function CapsulePage() {
   return <div>
     <h1>CapsulePage</h1>
     <div>
       <SearchBar />
+      <ProfileImgRegister />
+      <ProfileImg />
     </div>
   </div>;
 }
