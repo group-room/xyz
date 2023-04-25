c6c079998268168095a3e50471df168d2da7c8b5
송수현 <ssh1692@naver.com>
2023-04-25 14:19:21 +0900

    🎨 art: 알림페이지 친구 알림 목록 디자인 수정중

---
+++ frontend/xyz/src/components/notification/Notification.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Notification() {
+  const [isFriend, setIsFriend] = useState(false);
+
+  return (
+    <div className='w-full mt-4 border-2 border-black rounded-md'>
+      <div className='flex border-b-2 border-black h-10 w-full'>
+        <div className='flex items-center justify-center w-14 border-r-2 border-black'>
+          <p>마이룸</p>
+        </div>
+        <div className='w-[250px] flex items-center justify-center'><p>ㅇㅇㅇ님이 마이룸 알림입니다.</p></div>
+        <div className='w-10 border-l-2 border-black flex items-center justify-center'>
+          <Image src="/icons/check.svg" alt="checkImg" width={20} height={20} />
+        </div>
+      </div>
+
+      { isFriend ?         
+        <div className='flex h-10 items-center justify-center'>
+          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black'>수락</div>
+          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black'>거절</div>
+          <div className='flex items-center justify-center w-1/3'>차단</div>
+        </div> :
+        <div className='flex items-center justify-center  h-10 w-full'>
+          <div className='flex items-center justify-center w-1/2 h-10 border-r-2 border-black'>
+            <p>마이룸으로</p>
+          </div>
+          <div className='flex items-center justify-center w-1/2'>확인</div>
+        </div>      
+      }
+
+    </div>
+  );
+}
+
