72bd9039005c552b45dc4316c1e8cd3dc5ec3330
송수현 <ssh1692@naver.com>
2023-04-25 14:28:06 +0900

    🎨 art: 알림페이지 친구알림목록 디자인 수정중

--- frontend/xyz/src/components/notification/Notification.tsx
+++ frontend/xyz/src/components/notification/Notification.tsx
@@ -7,26 +7,26 @@ export default function Notification() {
   return (
     <div className='w-full mt-4 border-2 border-black rounded-md'>
       <div className='flex border-b-2 border-black h-10 w-full'>
-        <div className='flex items-center justify-center w-14 border-r-2 border-black'>
+        <div className='flex items-center justify-center w-14 border-r-2 border-black bg-slate-300'>
           <p>마이룸</p>
         </div>
         <div className='w-[250px] flex items-center justify-center'><p>ㅇㅇㅇ님이 마이룸 알림입니다.</p></div>
-        <div className='w-10 border-l-2 border-black flex items-center justify-center'>
+        <div className='w-10 border-l-2 border-black flex items-center justify-center bg-slate-300'>
           <Image src="/icons/check.svg" alt="checkImg" width={20} height={20} />
         </div>
       </div>
 
-      { isFriend ?         
+      { !isFriend ?         
         <div className='flex h-10 items-center justify-center'>
-          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black'>수락</div>
-          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black'>거절</div>
-          <div className='flex items-center justify-center w-1/3'>차단</div>
+          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black bg-yellow'>수락</div>
+          <div className='flex items-center justify-center w-1/3 h-10 border-r-2 border-black bg-pink'>거절</div>
+          <div className='flex items-center justify-center w-1/3 bg-slate-100 h-10'>차단</div>
         </div> :
         <div className='flex items-center justify-center  h-10 w-full'>
-          <div className='flex items-center justify-center w-1/2 h-10 border-r-2 border-black'>
+          <div className='flex items-center justify-center w-1/2 h-10 border-r-2 border-black bg-yellow'>
             <p>마이룸으로</p>
           </div>
-          <div className='flex items-center justify-center w-1/2'>확인</div>
+          <div className='flex items-center justify-center w-1/2 h-10 bg-slate-100'>확인</div>
         </div>      
       }
 
