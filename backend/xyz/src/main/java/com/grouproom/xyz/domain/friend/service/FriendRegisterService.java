bae0b5ab1db7bef38b77c96ceb73967c144005be
sairo <from415@kakao.com>
2023-04-25 11:39:19 +0900

    :sparkles: feat: 닉네임으로 사용자 검색

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendRegisterService.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendRegisterService.java
@@ -1,7 +1,10 @@
 package com.grouproom.xyz.domain.friend.service;
 
+import com.grouproom.xyz.domain.friend.dto.response.UserListResponse;
+
 public interface FriendRegisterService {
 
     // 닉네임 검색, 고유 코드 검색, 요청, 요청 취소, 요청 수락
+    UserListResponse findUserByNickname(Long loginSeq, String nickname);
     String saveFriendRequest(Long loginSeq, Long userSeq);
 }
