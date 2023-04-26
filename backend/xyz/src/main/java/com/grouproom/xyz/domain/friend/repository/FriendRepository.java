a658d32c4b30f02ba0c230096b6f0397878c36f7
sairo <from415@kakao.com>
2023-04-26 14:53:08 +0900

    :bug: fix: friend 도메인 repository JpaRepository<T, ID> 수정 (복합키이기 때문에 ID 부분 Id class 로 변경)

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/repository/FriendRepository.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/repository/FriendRepository.java
@@ -1,12 +1,13 @@
 package com.grouproom.xyz.domain.friend.repository;
 
 import com.grouproom.xyz.domain.friend.entity.Friend;
+import com.grouproom.xyz.domain.friend.entity.FriendID;
 import com.grouproom.xyz.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FriendRepository extends JpaRepository<Friend, Long>, FriendRepositoryCustom {
+public interface FriendRepository extends JpaRepository<Friend, FriendID>, FriendRepositoryCustom {
 
     Friend findByFromUserAndToUserAndIsAcceptedAndIsDeleted(User from, User to, Boolean isAccepted, Boolean isDeleted);
 
