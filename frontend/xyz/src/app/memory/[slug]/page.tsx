6757f0bf8d6e75387624ecaca0b662a88b5cc6b7
김경림 <myway01212@naver.com>
2023-04-26 10:30:35 +0900

    ♻️ refactor: album -> memory로 키값 변경 및 상세 페이지 골격 생성 (#S08P31A304-202)

---
+++ frontend/xyz/src/app/memory/[slug]/page.tsx
@@ -0,0 +1,13 @@
+type Props = {
+  params: {
+    slug: number;
+  };
+};
+
+import React from "react";
+
+function MemoryDetailPage({ params: { slug } }: Props) {
+  return <div>MemoryDetailPage</div>;
+}
+
+export default MemoryDetailPage;
