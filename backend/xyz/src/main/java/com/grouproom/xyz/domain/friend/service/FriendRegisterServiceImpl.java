36df858a051c1ae025cbbd854761f1f8ab632d17
sairo <from415@kakao.com>
2023-04-27 09:02:49 +0900

    :recycle: refactor: 친구 요청 취소 리팩토링 (JPA로 처리)

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendRegisterServiceImpl.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendRegisterServiceImpl.java
@@ -204,7 +204,7 @@ public class FriendRegisterServiceImpl implements FriendRegisterService {
 
         logger.info("cancelFriendRequest 호출");
 
-        Friend friend = friendRepository.findByFromUserAndToUser1(loginSeq, userSeq);
+        Friend friend = friendRepository.findByFromUser_SequenceAndToUser_SequenceAndIsAcceptedAndIsCanceledAndIsDeleted(loginSeq, userSeq, false, false, false);
         if(null == friend) {
             logger.severe("취소할 수 있는 대상이 아님");
             throw new RuntimeException();
