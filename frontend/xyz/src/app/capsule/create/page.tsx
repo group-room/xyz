c47df3035097526fc04915ad9f13d16dd6b97e4d
송수현 <ssh1692@naver.com>
2023-04-28 09:43:04 +0900

    Merge branch 'frontend' of https://lab.ssafy.com/s08-final/S08P31A304 into frontend

---
+++ frontend/xyz/src/app/capsule/create/page.tsx
@@ -0,0 +1,21 @@
+
+export default function page() {
+  return (
+    <div>
+      <h2>타임캡슐 만들기</h2>
+      <div >
+        그룹명 리스트
+      </div>
+      <div>
+        사진동영상
+      </div>
+      <div>
+        지도
+      </div>
+      <div>
+        추억 작성
+      </div>
+    </div>
+  );
+}
+
