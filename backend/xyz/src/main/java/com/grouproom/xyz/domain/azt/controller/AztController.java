408e86e645a6c6395f3576281adb0110bfea7b71
sairo <from415@kakao.com>
2023-05-01 14:02:32 +0900

    :bug: fix: 테스트를 위해 하드 코딩된 로그인 유저 정보 실제 로그인한 유저 정보로 변경

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/azt/controller/AztController.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/azt/controller/AztController.java
@@ -5,6 +5,7 @@ import com.grouproom.xyz.domain.azt.service.AztService;
 import com.grouproom.xyz.global.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,49 +22,49 @@ public class AztController {
     @GetMapping ("/all")
     public BaseResponse<?> aztList() {
         logger.info("aztList 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.findAztList(loginSeq));
     }
 
     @GetMapping ("/{aztSeq}")
     public BaseResponse<?> aztDetails(@PathVariable("aztSeq") Long aztSeq) {
         logger.info("aztDetails 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.findAzt(loginSeq, aztSeq));
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public BaseResponse<?> addAzt(@RequestPart AztRequest aztRequest, @RequestPart(required = false) MultipartFile image) {
         logger.info("addAzt 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.addAzt(loginSeq, aztRequest, image));
     }
 
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public BaseResponse<?> modifyAzt(@RequestPart AztRequest aztRequest, @RequestPart(required = false) MultipartFile image) {
         logger.info("modifyAzt 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.modifyAzt(loginSeq, aztRequest, image));
     }
 
     @GetMapping("/friend/all")
     public BaseResponse<?> friendList(@RequestParam Long aztSeq) {
         logger.info("friendList 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.findFriendForMembers(loginSeq, aztSeq));
     }
 
     @PostMapping("/member")
     public BaseResponse<?> addAztMember(@RequestBody AztRequest aztRequest) {
         logger.info("addAztMember 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.addAztMember(loginSeq, aztRequest));
     }
 
     @DeleteMapping("/member/{aztSeq}")
     public BaseResponse<?> modifyAztMemberToDelete(@PathVariable("aztSeq") Long aztSeq) {
         logger.info("modifyAztMemberToDelete 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(aztService.modifyAztMemberToDelete(loginSeq, aztSeq));
     }
 
--- backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/controller/FriendController.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/controller/FriendController.java
@@ -6,6 +6,7 @@ import com.grouproom.xyz.domain.friend.service.FriendRegisterService;
 import com.grouproom.xyz.domain.friend.service.UserBlockService;
 import com.grouproom.xyz.global.model.BaseResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
@@ -23,77 +24,77 @@ public class FriendController {
     @GetMapping("/all")
     public BaseResponse<?> friendList() {
         logger.info("frinedList 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendManageService.findFriend(loginSeq));
     }
 
     @DeleteMapping("/{userSeq}")
     public BaseResponse<?> modifyFriendToDeleted(@PathVariable("userSeq") Long userSeq) {
         logger.info("modifyFriendDelete 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendManageService.modifyFriendToDeleted(loginSeq, userSeq));
     }
 
     @GetMapping()
     public BaseResponse<?> findFriendByNickname(@RequestParam String nickname) {
         logger.info("findFriendByNickname 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendManageService.findFriendByNickname(loginSeq, nickname));
     }
 
     @GetMapping("/{identify}")
     public BaseResponse<?> findFriendByIdentify(@PathVariable("identify") String identify) {
         logger.info("findFriendByIdentify 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendManageService.findFriendByIdentify(loginSeq, identify));
     }
 
     @GetMapping("/user")
     public BaseResponse<?> findUserByNickname(@RequestParam String nickname) {
         logger.info("findUserByNickname 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendRegisterService.findUserByNickname(loginSeq, nickname));
     }
 
     @GetMapping("/user/{identify}")
     public BaseResponse<?> findUserByIdentify(@PathVariable("identify") String identify) {
         logger.info("findUserByIdentify 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendRegisterService.findUserByIdentify(loginSeq, identify));
     }
 
     @PostMapping()
     public BaseResponse<?> saveFriend(@RequestBody FriendRequest friendRequest) {
         logger.info("saveFriendRequest 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendRegisterService.saveFriend(loginSeq, friendRequest.getUserSeq()));
     }
 
     @PutMapping("/cancel")
     public BaseResponse<?> modifyFriendToCancel(@RequestBody FriendRequest friendRequest) {
         logger.info("cancelFriendRequest 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendRegisterService.modifyFriendToCancel(loginSeq, friendRequest.getUserSeq()));
     }
 
     @PutMapping("/accept")
     public BaseResponse<?> modifyFriendToAccept(@RequestBody FriendRequest friendRequest) {
         logger.info("modifyFriendToAccept 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(friendRegisterService.modifyFriendToAccept(loginSeq, friendRequest.getUserSeq()));
     }
 
     @PostMapping("/block")
     public BaseResponse<?> saveUserBlock(@RequestBody FriendRequest friendRequest) {
         logger.info("saveUserBlock 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(userBlockService.saveUserBlock(loginSeq, friendRequest.getUserSeq()));
     }
 
     @DeleteMapping("/block/{userSeq}")
     public BaseResponse<?> modifyUserBlock(@PathVariable("userSeq") Long userSeq) {
         logger.info("modifyUserBlock 호출");
-        Long loginSeq = 1L;
+        Long loginSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
         return new BaseResponse<>(userBlockService.modifySaveBlock(loginSeq, userSeq));
     }
 }
