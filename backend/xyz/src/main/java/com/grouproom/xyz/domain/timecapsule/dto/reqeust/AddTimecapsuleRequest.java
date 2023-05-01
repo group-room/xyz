e21e708e4829b932fe055576a874d5912c7ff436
yujj25 <yujj25@naver.com>
2023-05-01 14:01:28 +0900

    :art: add: timecapsule Controller, Repository, Service 생성

---
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/timecapsule/dto/reqeust/AddTimecapsuleRequest.java
@@ -0,0 +1,4 @@
+package com.grouproom.xyz.domain.timecapsule.dto.reqeust;
+
+public class AddTimecapsuleRequest {
+}
---
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/timecapsule/dto/response/AddTimecapsuleResponse.java
@@ -0,0 +1,4 @@
+package com.grouproom.xyz.domain.timecapsule.dto.response;
+
+public class AddTimecapsuleResponse {
+}
