c7c36f6f1cdb497adf45c9b7b1d029eaa5eb9e4e
sairo <from415@kakao.com>
2023-05-01 15:10:29 +0900

    :memo: docs: 아지트 도메인 스웨거 설명 추가 (#S08P31A304-292)

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/azt/controller/AztController.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/azt/controller/AztController.java
@@ -3,6 +3,7 @@ package com.grouproom.xyz.domain.azt.controller;
 import com.grouproom.xyz.domain.azt.dto.request.AztRequest;
 import com.grouproom.xyz.domain.azt.service.AztService;
 import com.grouproom.xyz.global.model.BaseResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -19,6 +20,7 @@ public class AztController {
     private final Logger logger = Logger.getLogger("com.grouproom.xyz.domain.azt.controller.AztController");
     private final AztService aztService;
 
+    @ApiOperation(value = "아지트 목록 조회", notes = "로그인한 사용자가 소속된 아지트 목록을 반환한다.")
     @GetMapping ("/all")
     public BaseResponse<?> aztList() {
         logger.info("aztList 호출");
@@ -26,6 +28,7 @@ public class AztController {
         return new BaseResponse<>(aztService.findAztList(loginSeq));
     }
 
+    @ApiOperation(value = "아지트 상세 조회", notes = "선택한 아지트의 상세 정보를 반환한다.")
     @GetMapping ("/{aztSeq}")
     public BaseResponse<?> aztDetails(@PathVariable("aztSeq") Long aztSeq) {
         logger.info("aztDetails 호출");
@@ -33,6 +36,7 @@ public class AztController {
         return new BaseResponse<>(aztService.findAzt(loginSeq, aztSeq));
     }
 
+    @ApiOperation(value = "아지트 생성", notes = "아지트 생성 후 해당 아지트의 상세 정보를 반환한다. (name required, aztSeq not used)")
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public BaseResponse<?> addAzt(@RequestPart AztRequest aztRequest, @RequestPart(required = false) MultipartFile image) {
         logger.info("addAzt 호출");
@@ -40,6 +44,7 @@ public class AztController {
         return new BaseResponse<>(aztService.addAzt(loginSeq, aztRequest, image));
     }
 
+    @ApiOperation(value = "아지트 수정", notes = "아지트 이름/대표사진을 수정 후 해당 아지트의 상세 정보를 반환한다. (aztSeq required, members not used)")
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     public BaseResponse<?> modifyAzt(@RequestPart AztRequest aztRequest, @RequestPart(required = false) MultipartFile image) {
         logger.info("modifyAzt 호출");
@@ -47,6 +52,7 @@ public class AztController {
         return new BaseResponse<>(aztService.modifyAzt(loginSeq, aztRequest, image));
     }
 
+    @ApiOperation(value = "초대할 수 있는 친구 목록 조회", notes = "선택한 아지트에 초대할 수 있는 친구 목록을 반환한다.")
     @GetMapping("/friend/all")
     public BaseResponse<?> friendList(@RequestParam Long aztSeq) {
         logger.info("friendList 호출");
@@ -54,6 +60,7 @@ public class AztController {
         return new BaseResponse<>(aztService.findFriendForMembers(loginSeq, aztSeq));
     }
 
+    @ApiOperation(value = "멤버 초대", notes = "선택한 아지트에 선택한 멤버들을 추가한 후 해당 아지트 상세 정보를 반환한다. (aztseq/members required, name/image not used)")
     @PostMapping("/member")
     public BaseResponse<?> addAztMember(@RequestBody AztRequest aztRequest) {
         logger.info("addAztMember 호출");
@@ -61,6 +68,7 @@ public class AztController {
         return new BaseResponse<>(aztService.addAztMember(loginSeq, aztRequest));
     }
 
+    @ApiOperation(value = "아지트 탈퇴", notes = "로그인한 사용자를 선택한 아지트에서 삭제 후 아지트 목록을 반환한다.")
     @DeleteMapping("/member/{aztSeq}")
     public BaseResponse<?> modifyAztMemberToDelete(@PathVariable("aztSeq") Long aztSeq) {
         logger.info("modifyAztMemberToDelete 호출");
