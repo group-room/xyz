6a50654effa6e732a8991589272ef82b391da141
leeyuhmin <yuhmin92@naver.com>
2023-05-02 16:36:34 +0900

    :sparkles: feat: 로그인 페이지 css(#S08P31A304-252)

--- frontend/xyz/src/app/page.tsx
+++ frontend/xyz/src/app/page.tsx
@@ -1,48 +1,54 @@
 import Image from "next/image";
 import Link from "next/link";
-import KakaoLogin from "../../public/images/login/kakao_login.png";
-import GoogleLogin from "../../public/images/login/google_login.png";
 import Rectangle from "../../public/icons/rectangle.svg";
 import Close from "../../public/icons/close.svg";
 import Logo from "../../public/images/logo.svg";
+import Kakao from "../../public/icons/kakao.svg";
+import Naver from "../../public/icons/naver.svg";
+import Google from "../../public/icons/google.svg";
 
 export default function Home() {
   return (
     <section>
       <div className="flex flex-row-reverse py-3 gap-2 pr-2 rounded-t-md fixed top-0 left-0 border border-black bg-blue w-full ">
-        <div className="">
+        <div>
           <Image src={Close} alt="닫기" width={20} height={20} />
         </div>
-        <div className="">
+        <div>
           <Image src={Rectangle} alt="최대화" width={20} height={20} />
         </div>
         <div className=" border-b-2 border-black w-5 "></div>
       </div>
 
-      <div className=" fixed top-[242px] left-[82px] items-center justify-center w-full h-full">
-        <Image src={Logo} alt="로고" />
+      <div className=" mt-60 mb-28 flex justify-center w-full h-full">
+        <Image src={Logo} alt="로고" width={234} height={80} />
       </div>
-      <Link href={"https://xyz-gen.com/backend/api/user/login/kakao"}>
-        <div className=" fixed top-[439px] left-[69px] w-[255px] h-[66px] rounded-[5px] bg-[#fddc3f] border border-black ">
-          <div className="fixed top-[459px] w-[320px] h-16 text-xl text-center text-black">
-            카카오 로그인
+      <div className=" flex flex-col gap-8 justify-center items-center ">
+        <Link href={"https://xyz-gen.com/backend/api/user/login/kakao"}>
+          <div className=" flex flex-row justify-center items-center gap-4 w-[255px] h-[66px] rounded-[5px] bg-[#fddc3f] border border-black ">
+            <div>
+              <Image src={Kakao} alt="카카오 로고" width={30} height={30} />
+            </div>
+            <div className="text-xl text-black">카카오 로그인</div>
           </div>
-        </div>
-      </Link>
-      <Link href={"https://xyz-gen.com/backend/api/user/login/naver"}>
-        <div className=" fixed top-[525px] left-[69px] w-[255px] h-[66px] rounded-[5px] bg-[#1dc800] border border-black">
-          <div className="w-[191px] h-16 text-xl text-center text-white">
-            네이버 로그인
+        </Link>
+        <Link href={"https://xyz-gen.com/backend/api/user/login/naver"}>
+          <div className="flex flex-row justify-center items-center gap-4 w-[255px] h-[66px] rounded-[5px] bg-[#1dc800] border border-black">
+            <div>
+              <Image src={Naver} alt="네이버 로고" width={30} height={30} />
+            </div>
+            <div className="text-xl  text-white">네이버 로그인</div>
           </div>
-        </div>
-      </Link>
-      <Link href={"https://xyz-gen.com/backend/api/user/login/google"}>
-        <div className=" fixed top-[621px] left-[69px] w-[255px] h-[66px] rounded-[5px] bg-white border border-black">
-          <div className="w-[191px] h-16 text-xl text-center text-black">
-            구글 로그인
+        </Link>
+        <Link href={"https://xyz-gen.com/backend/api/user/login/google"}>
+          <div className="flex flex-row justify-center items-center gap-4 w-[255px] h-[66px] rounded-[5px] bg-white border border-black">
+            <div>
+              <Image src={Google} alt="구글 로고" width={22} height={22} />
+            </div>
+            <div className="text-xl  text-black">구글 로그인</div>
           </div>
-        </div>
-      </Link>
+        </Link>
+      </div>
     </section>
   );
 }
