39f7590e36141b83441caef7f6d696c62521c7d5
김경림 <myway01212@naver.com>
2023-05-03 17:33:08 +0900

    🎨 add: 댓글 삭제전 모달 확인 추가 (#S08P31A304-241)

--- frontend/xyz/src/components/memory/CommentItem.tsx
+++ frontend/xyz/src/components/memory/CommentItem.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteMemoryComment } from "@/app/api/memory";
 import { queryKeys } from "@/constants/queryKeys";
 import ProfileImg from "../common/ProfileImg";
+import ModalBtn from "../common/ModalBtn";
 
 interface CommentItemProps {
   commentSeq: number;
@@ -25,32 +28,48 @@ function CommentItem({
     mutationFn: () => deleteMemoryComment(commentSeq),
     onSuccess: () => {
       queryClient.invalidateQueries(queryKeys.memory.memoryDetail(memorySeq));
+      alert("댓글이 삭제되었어요.");
     },
   });
-  const handleDeleteClick = () => {
+
+  const [isModal, setIsModal] = useState(false);
+  const handleClickModalYes = () => {
     useDeleteMemoryCommentMutation.mutate();
   };
 
+  const handleDeleteClick = () => {
+    setIsModal(true);
+  };
+
   return (
-    <div className="flex gap-x-3 py-3">
-      <div className="flex-none">
-        <ProfileImg imgSrc={profileImage} />
-      </div>
-      <div className="flex flex-col w-full">
-        <div className="flex justify-between pr-2">
-          <p className="mb-1 mt-2">{nickname} :</p>
-          <button onClick={handleDeleteClick}>
-            <Image
-              src="/icons/close.svg"
-              alt="삭제 아이콘"
-              width={12}
-              height={12}
-            />
-          </button>
+    <>
+      <div className="flex gap-x-3 py-3">
+        <div className="flex-none">
+          <ProfileImg imgSrc={profileImage} />
         </div>
-        <p>{content}</p>
+        <div className="flex flex-col w-full">
+          <div className="flex justify-between pr-2">
+            <p className="mb-1 mt-2">{nickname} :</p>
+            <button onClick={handleDeleteClick}>
+              <Image
+                src="/icons/close.svg"
+                alt="삭제 아이콘"
+                width={12}
+                height={12}
+              />
+            </button>
+          </div>
+          <p>{content}</p>
+        </div>
       </div>
-    </div>
+      {isModal && (
+        <ModalBtn
+          yesFunc={handleClickModalYes}
+          closeModal={() => setIsModal(false)}
+          text="댓글을 삭제할까요?"
+        />
+      )}
+    </>
   );
 }
 
