4c1bda7ad2cf1e2dacecfce2da8cdb1acd30ce0d
leeyuhmin <yuhmin92@naver.com>
2023-05-03 14:57:28 +0900

    :sparkles: feat: 모달 컴포넌트 버튼 있는 버전 (#S08P31A304-250)

--- frontend/xyz/src/app/profile/page.tsx
+++ frontend/xyz/src/app/profile/page.tsx
@@ -1,21 +1,47 @@
 "use client";
-import Modal from "@/components/common/Modal";
-import Tab from "@/components/common/Tab";
-import Textbox from "@/components/common/Textbox";
+import Modal from "../../components/common/ModalBtn";
 import Btn from "@/components/common/Btn";
 import { BgColors } from "@/constants/style";
 import React from "react";
 import { useState } from "react";
+import ProfileMain from "../../components/profile/ProfileMain";
 
 function ProfilePage() {
   const [isModal, setIsModal] = useState(false);
   const buttonClick = () => {};
+  const buttonYes = () => {
+    alert("친구가 되었습니다.");
+  };
 
   const handleClick = () => {
     setIsModal(true);
   };
+  return (
+    <div className="w-full h-full">
+      <div className="flex pt-5">
+        <div>
+          <Btn width="w-40" bgColor="blue" text="친 구" btnFunc={handleClick} />
+        </div>
+        <div className=" pl-7">
+          <Btn
+            width="w-40"
+            bgColor="blue"
+            text="나의 활동"
+            btnFunc={buttonClick}
+          />
+        </div>
+      </div>
 
-  return <div className="w-full h-full"></div>;
+      <div onClick={handleClick}> ProfilePage</div>
+      {isModal && (
+        <Modal
+          yesFunc={buttonYes}
+          closeModal={() => setIsModal(false)}
+          text="나랑 친구를 해볼래? 나랑 친구를 해볼래?"
+        />
+      )}
+    </div>
+  );
 }
 
 export default ProfilePage;
