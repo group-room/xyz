b7ca90bfa2d2d881545482c6558cabe289068a2d
김경림 <myway01212@naver.com>
2023-05-04 12:47:25 +0900

    ✨ feat: 탭바 메뉴 선택시 표시 스타일 추가 (#S08P31A304-180)

--- frontend/xyz/src/components/TabBar.tsx
+++ frontend/xyz/src/components/TabBar.tsx
@@ -1,14 +1,45 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function TabBar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <div className="fixed flex items-center justify-evenly bottom-0 left-0 right-0 h-14 bg-white shadow-[0_-3px_20px_-20px_rgba(0,0,0,0.3)] z-50">
-      <Link href={"/memory"}>추억</Link>
-      <Link href={"/capsule"}>캡슐</Link>
-      <Link href={"/azt"}>아지트</Link>
-      <Link href={"/chat"}>채팅</Link>
-      <Link href={"/profile"}>프로필</Link>
+      <Link
+        href={"/memory"}
+        className={pathname.includes("/memory") ? "text-pink" : ""}
+      >
+        추억
+      </Link>
+      <Link
+        href={"/capsule"}
+        className={pathname.includes("/capsule") ? "text-pink" : ""}
+      >
+        캡슐
+      </Link>
+      <Link
+        href={"/azt"}
+        className={pathname.includes("/azt") ? "text-pink" : ""}
+      >
+        아지트
+      </Link>
+      <Link
+        href={"/chat"}
+        className={pathname.includes("/chat") ? "text-pink" : ""}
+      >
+        채팅
+      </Link>
+      <Link
+        href={"/profile"}
+        className={pathname.includes("/profile") ? "text-pink" : ""}
+      >
+        프로필
+      </Link>
     </div>
   );
 }
