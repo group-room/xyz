9af7e3231e46aac1e42d3eee07021730c2381264
김경림 <myway01212@naver.com>
2023-05-04 10:19:38 +0900

    🐛 fix: 추억 편집시 사진 다시 선택했을때 지도 현재위치 못가져오는 버그 수정 (#S08P31A304-240)

--- frontend/xyz/src/components/memory/KakaoMap.tsx
+++ frontend/xyz/src/components/memory/KakaoMap.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { convertAddress } from "@/app/api/kakao";
 import { KakaoMapProps } from "@/types/memory";
 
+/** 추억앨범 피드, 추억사진 업로드, 편집, 상세보기 페이지에서 사용하는 지도 컴포넌트 */
 function KakaoMap({
   height,
   position,
@@ -39,12 +40,14 @@ function KakaoMap({
   };
 
   useEffect(() => {
-    // 현재 위치 조회하기
+    // 현재 위치 조회하기 (사진 편집 처음 진입시에는 기존 위치)
+    // console.log("현재 위치 조회하기");
     navigator.geolocation.getCurrentPosition(successHandler, errorHandler); // 성공시 successHandler, 실패시 errorHandler 함수 실행
   }, []);
 
-  // 마커 찍을 때마다 위치 가져와서 변환하기
   useEffect(() => {
+    // 마커 찍을 때마다 위치 가져와서 변환하기
+    // console.log("포지션 변경");
     if (position.lat !== 0 && position.lng !== 0) {
       getConvertedAddress(position.lng.toString(), position.lat.toString());
     } else {
@@ -54,14 +57,15 @@ function KakaoMap({
 
   const successHandler = (response: any) => {
     // console.log(response); // coords: GeolocationCoordinates {latitude: 위도, longitude: 경도, …} timestamp: 1673446873903
-    const { latitude, longitude } = response.coords;
+
+    const { latitude, longitude } = response.coords; // 현재 위치 정보
     setCurrLocation({ lat: latitude, lng: longitude });
-    if (isPhotoUpload && !isPhotoEdit) {
-      // 추억 처음 등록하는 거라면 (편집 X) 현재 위치 없애고 사진 메타데이터 위치 or 수정해서 마커 찍은 위치로 이동하도록 하기}
+
+    // 추억 처음 등록하는 거라면 현재 위치 대신 사진 메타데이터 위치 or 마커 찍은 위치로 이동하도록 하기
+    if (!isPhotoEdit) {
       setPosition({ lat: latitude, lng: longitude });
       getConvertedAddress(longitude.toString(), latitude.toString());
     }
-    getConvertedAddress(longitude.toString(), latitude.toString());
   };
 
   const errorHandler = (error: any) => {
