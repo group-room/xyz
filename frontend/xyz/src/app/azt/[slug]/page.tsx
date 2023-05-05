841d9b525ceb5be8bb25ab3339557cfa6627302e
skylar121 <myway01212@naver.com>
2023-05-05 23:52:12 +0900

    ✨ feat: 아지트 상세 타임캡슐 조회 (#S08P31A304-299)

--- frontend/xyz/src/app/azt/[slug]/page.tsx
+++ frontend/xyz/src/app/azt/[slug]/page.tsx
@@ -8,14 +8,18 @@ import ToggleBtn from "@/components/common/ToggleBtn";
 import Textbox from "@/components/common/Textbox";
 import Image from "next/image";
 import Link from "next/link";
+import { useAztCapsuleList } from "@/hooks/queries/capsule";
+import { API } from "@/constants/queryKeys";
 
 function AzitDetailPage({ params: { slug } }: SlugProps) {
   const { data: aztMemoryData, isLoading: isAztMemoryLoading } = useMemoryList({
     date: null,
     aztSeq: slug,
   });
-  const [btnValue, setBtnValue] = useState(true);
+  const { data: aztCapsuleData, isLoading: isAztCapsuleLoading } =
+    useAztCapsuleList(slug);
 
+  const [btnValue, setBtnValue] = useState(true);
   const handleChange = (selectedBtn: boolean) => {
     setBtnValue(selectedBtn);
   };
@@ -40,9 +44,17 @@ function AzitDetailPage({ params: { slug } }: SlugProps) {
       if (isAztMemoryLoading) return <div>로딩중...</div>;
       return (
         <>
-          {aztMemoryData && aztMemoryData.length ? (
-            aztMemoryData.map((memory) => (
-              <MemoryItem key={memory.memorySeq} memory={memory} />
+          {aztCapsuleData && aztCapsuleData.length ? (
+            aztCapsuleData.map((capule) => (
+              // TODO: 타임캡슐 컴포넌트 만들어지면 수정
+              // <MemoryItem key={capule.tcSeq} />
+              <Image
+                key={capule.tcSeq}
+                src="/images/capsule.svg"
+                alt="capsuleImg"
+                width={500}
+                height={500}
+              />
             ))
           ) : (
             <div>
@@ -61,7 +73,7 @@ function AzitDetailPage({ params: { slug } }: SlugProps) {
           <Textbox icon="/icons/users.svg" alt="아지트 아이콘" bgColor="pink" />
         </div>
         <div className="flex gap-x-2 items-center">
-          <Link href={`chat/${slug}`}>
+          <Link href={`/${API.chat}/${slug}`}>
             <Image
               src="/icons/chat.svg"
               alt="아지트 채팅"
@@ -69,7 +81,7 @@ function AzitDetailPage({ params: { slug } }: SlugProps) {
               height={20}
             />
           </Link>
-          <Link href={`azt/${slug}/info`}>
+          <Link href={`/${API.azt}/${slug}/info`}>
             <Image
               src="/icons/info.svg"
               alt="아지트 상세정보"
@@ -80,8 +92,10 @@ function AzitDetailPage({ params: { slug } }: SlugProps) {
         </div>
       </div>
       <ToggleBtn
-        textL={`추억앨범 (${aztMemoryData?.length})`}
-        textR="타임캡슐"
+        textL={`추억앨범 ${aztMemoryData ? `(${aztMemoryData?.length})` : ""}`}
+        textR={`타임캡슐 ${
+          aztCapsuleData ? `(${aztCapsuleData?.length})` : ""
+        }`}
         imgL="/icons/folder.svg"
         imgR="/icons/folder.svg"
         value={btnValue}
