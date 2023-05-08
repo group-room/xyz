0599ab42ecf5561f42e21d1b2db9a410d8d1dc9a
yujj25 <yujj25@naver.com>
2023-05-08 14:27:25 +0900

    :sparkles: feat: SSE 로직 추가 (#S08P31A304-325)

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/service/NotificationService.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/service/NotificationService.java
@@ -1,10 +1,13 @@
 package com.grouproom.xyz.domain.notification.service;
 
 import com.grouproom.xyz.domain.notification.dto.response.NotificationListResponse;
+import com.grouproom.xyz.domain.notification.entity.Notification;
 
 public interface NotificationService {
 
     NotificationListResponse findNotificationList(Long userSeq, String type);
 
     void removeNotification(Long userSeq, Long notificationSeq);
+
+    void notifyEvent(Notification notification);
 }
--- backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/service/NotificationServiceImpl.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/service/NotificationServiceImpl.java
@@ -5,20 +5,25 @@ import com.grouproom.xyz.domain.notification.dto.response.NotificationResponse;
 import com.grouproom.xyz.domain.notification.entity.Notification;
 import com.grouproom.xyz.domain.notification.entity.NotificationType;
 import com.grouproom.xyz.domain.notification.repository.NotificationRepository;
+import com.grouproom.xyz.domain.user.repository.UserRepository;
 import com.grouproom.xyz.global.exception.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
+import static com.grouproom.xyz.domain.notification.controller.SseController.sseEmitters;
+
 @Service
 @RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
+    private final UserRepository userRepository;
     private final NotificationRepository notificationRepository;
     private final Logger logger = Logger.getLogger("com.grouproom.xyz.domain.notification.service.NotificationServiceImpl");
 
@@ -39,7 +44,7 @@ public class NotificationServiceImpl implements NotificationService {
 
         for (Notification notification : notifications) {
             notificationResponses.add(new NotificationResponse(notification));
-            if (notification.getIsReceived() == false) {
+            if (!notification.getIsReceived()) {
                 notification.updateIsReceived();
             }
         }
@@ -61,5 +66,19 @@ public class NotificationServiceImpl implements NotificationService {
         }
 
         notification.updateIsDeleted();
+    }
+
+    @Override
+    @Transactional
+    public void notifyEvent(Notification notification) {
+        Long userSeq = notification.getUser().getSequence();
+        if (sseEmitters.containsKey(userSeq)) {
+            SseEmitter sseEmitter = sseEmitters.get(userSeq);
+            try {
+                sseEmitter.send(SseEmitter.event().name("newNotification").data("새로운 알림이 있습니다."));
+            } catch (Exception e) {
+                sseEmitters.remove(userSeq);
+            }
+        }
     }
 }
