2a9c4936e4811903a91ba80275addde90c9c9b39
송수현 <ssh1692@naver.com>
2023-05-09 17:11:16 +0900

    ✨ feat: 알림목록리스트구현 - 알림 삭제 DELETE 완성

--- frontend/xyz/src/app/api/instance.ts
+++ frontend/xyz/src/app/api/instance.ts
@@ -7,7 +7,7 @@ const state = store.getState();
 export const axiosInstance = axios.create({
   baseURL: SERVER_URL,
   headers: {
-    Authorization: state.auth.accessToken,
+    Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXF1ZW5jZSI6MSwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTY4MzUwNjk1OSwiZXhwIjoxNjgzNjc5NzU5fQ.VT4DoLqgq3Nt6Ep8S2Rz60uiSSI2p1ShYqFD3LYw-4w",
   }, // 나중에 토큰 넣어주기
   withCredentials: true,
 });
@@ -16,7 +16,7 @@ export const axiosFileInstance = axios.create({
   baseURL: SERVER_URL,
   headers: {
     "Content-type": "multipart/form-data",
-    Authorization: state.auth.accessToken,
+    Authorization: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXF1ZW5jZSI6MSwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTY4MzUwNjk1OSwiZXhwIjoxNjgzNjc5NzU5fQ.VT4DoLqgq3Nt6Ep8S2Rz60uiSSI2p1ShYqFD3LYw-4w",
   }, // 나중에 토큰 넣어주기
   withCredentials: true,
 });
