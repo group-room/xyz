fe12fe9519ce4c6d3ec89fcda344feb5950139ba
송수현 <ssh1692@naver.com>
2023-05-10 16:30:59 +0900

    ✨ feat: 타임캡슐생성페이지 - 타임캡슐 저장 위치 지도 생성 완료

--- frontend/xyz/src/components/timecapsule/SearchPostCode.tsx
+++ frontend/xyz/src/components/timecapsule/SearchPostCode.tsx
@@ -1,12 +1,17 @@
 import { convertAddressToCoordinate } from "@/app/api/kakao";
-import { CoordinateTypes } from "@/types/capsule";
-import React from "react";
+import { CoordinateTypes, positionTypes } from "@/types/capsule";
+import React, { useState } from "react";
 import { useDaumPostcodePopup } from "react-daum-postcode";
+import { StaticMap } from "react-kakao-maps-sdk";
 
 export default function SearchPostCode() {
   const scriptUrl =
     "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
   const open = useDaumPostcodePopup(scriptUrl);
+  const [position, setPosition] = useState<positionTypes>({
+    lat: 0,
+    lng: 0,
+  });
 
   const handleComplete = async (data: any) => {
     let fullAddress = data.address;
@@ -31,6 +36,8 @@ export default function SearchPostCode() {
     );
     // addressInfo.x = longitude, addressInfo.y = latitude
     console.log(addressInfo);
+    setPosition({ lat: +addressInfo.y, lng: +addressInfo.x });
+    console.log(position);
   };
 
   const handleClick = () => {
@@ -38,8 +45,23 @@ export default function SearchPostCode() {
   };
 
   return (
-    <button type="button" onClick={handleClick}>
-      타임캡슐 저장 위치 검색
-    </button>
+    <div>
+      <button type="button" onClick={handleClick}>
+        타임캡슐 저장 위치 찾기
+      </button>
+      {position !== undefined && position.lat !== 0 && position.lng !== 0 && (
+        <StaticMap // 지도를 표시할 Container
+          // 지도의 중심좌표
+          center={position}
+          style={{
+            // 지도의 크기
+            width: "100%",
+            height: "150px",
+          }}
+          marker={{ position }}
+          level={3} // 지도의 확대 레벨
+        />
+      )}
+    </div>
   );
 }
--- frontend/xyz/src/types/capsule.ts
+++ frontend/xyz/src/types/capsule.ts
@@ -13,16 +13,21 @@ export type CapsuleAztTypes = {
 };
 
 export type CoordinateTypes = {
-  address_name: string;
-  b_code: string;
-  h_code: string;
-  main_address_no: string;
-  mountain_yn: string;
-  region_1depth_name: string;
-  region_2depth_name: string;
-  region_3depth_h_name: string;
-  region_3depth_name: string;
-  sub_address_no: string | null;
+  address_name?: string;
+  b_code?: string;
+  h_code?: string;
+  main_address_no?: string;
+  mountain_yn?: string;
+  region_1depth_name?: string;
+  region_2depth_name?: string;
+  region_3depth_h_name?: string;
+  region_3depth_name?: string;
+  sub_address_no?: string | null;
   x: string;
   y: string;
 };
+
+export type positionTypes = {
+  lat: number;
+  lng: number;
+}
