7a4d61271122560b04c83ec5e42c9034e07c7c7d
yujj25 <yujj25@naver.com>
2023-05-16 13:36:13 +0900

    :recycle: refactor: sse 연결 시도

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/controller/SseController.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/controller/SseController.java
@@ -3,6 +3,7 @@ package com.grouproom.xyz.domain.notification.controller;
 import com.grouproom.xyz.domain.notification.service.SseService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -27,12 +28,9 @@ public class SseController {
     @GetMapping(consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect() {
         logger.info("connect 호출");
-
         Long userSeq = Long.parseLong(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString());
-
         SseEmitter emitter = new SseEmitter();
         sseService.addSseEmitter(userSeq, emitter);
-
         try {
             emitter.send(SseEmitter.event()
                     .name("connect")
@@ -40,17 +38,19 @@ public class SseController {
             logger.info("sse send success");
         } catch (IOException e) {
             logger.info("sse send fail");
-            throw new RuntimeException(e);
+            sseService.removeSseEmitter(userSeq);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
-
         HttpHeaders headers = new HttpHeaders();
         headers.set("Connection", "keep-alive");
-
+        headers.add("Cache-Control", "no-store");
+        headers.add("Content-Type", "text/event-stream");
+        headers.add("Access-Control-Allow-Origin", "*");
         emitter.onCompletion(() -> sseService.removeSseEmitter(userSeq));
         emitter.onTimeout(() -> sseService.removeSseEmitter(userSeq));
         emitter.onError(e -> sseService.removeSseEmitter(userSeq));
-
-        return ResponseEntity.ok().headers(headers).body(emitter);    }
+        return ResponseEntity.ok().headers(headers).body(emitter);
+    }
 
 //    @CrossOrigin
 //    @GetMapping(consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
