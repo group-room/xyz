b059efa441af32e859fee5849a27908f6388d80d
yujj25 <yujj25@naver.com>
2023-05-16 13:48:03 +0900

    :recycle: refactor: sse 연결 시도

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/controller/SseController.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/notification/controller/SseController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
 import java.util.logging.Logger;
 
 @RestController
@@ -22,7 +23,8 @@ import java.util.logging.Logger;
 public class SseController {
 
     private final SseService sseService;
-//    ExecutorService sseMvcExecutor = Executors.newSingleThreadExecutor();
+//    Executors.newSingleThreadExecutor()
+
     private final Logger logger = Logger.getLogger("com.grouproom.xyz.domain.notification.controller.SseController");
 
     @GetMapping(consumes = MediaType.ALL_VALUE, produces = MediaType.TEXT_EVENT_STREAM_VALUE)
@@ -34,7 +36,7 @@ public class SseController {
         try {
             emitter.send(SseEmitter.event()
                     .name("connect")
-                    .data("connected!"));
+                    .data("connected!\n\n"));
             logger.info("sse send success");
         } catch (IOException e) {
             logger.info("sse send fail");
@@ -45,7 +47,7 @@ public class SseController {
         headers.set("Connection", "keep-alive");
         headers.add("Cache-Control", "no-store");
         headers.add("Content-Type", "text/event-stream");
-        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Origin", "http://localhost:3000");
         emitter.onCompletion(() -> sseService.removeSseEmitter(userSeq));
         emitter.onTimeout(() -> sseService.removeSseEmitter(userSeq));
         emitter.onError(e -> sseService.removeSseEmitter(userSeq));
