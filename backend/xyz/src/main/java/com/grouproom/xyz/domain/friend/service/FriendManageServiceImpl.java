d1a2b8873fc234d5e6b610fd5cbdbfd89c6e6271
yujj25 <yujj25@naver.com>
2023-05-17 09:51:12 +0900

    :art: add: 친구 끊기 시 알림 처리

--- backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendManageServiceImpl.java
+++ backend/xyz/src/main/java/com/grouproom/xyz/domain/friend/service/FriendManageServiceImpl.java
@@ -4,6 +4,10 @@ import com.grouproom.xyz.domain.friend.dto.response.FriendListResponse;
 import com.grouproom.xyz.domain.friend.dto.response.FriendUserResponse;
 import com.grouproom.xyz.domain.friend.entity.Friend;
 import com.grouproom.xyz.domain.friend.repository.FriendRepository;
+import com.grouproom.xyz.domain.notification.entity.Notification;
+import com.grouproom.xyz.domain.notification.entity.NotificationType;
+import com.grouproom.xyz.domain.notification.repository.NotificationRepository;
+import com.grouproom.xyz.domain.notification.service.NotificationService;
 import com.grouproom.xyz.global.exception.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,6 +22,8 @@ import java.util.logging.Logger;
 public class FriendManageServiceImpl implements FriendManageService {
 
     private final FriendRepository friendRepository;
+    private final NotificationService notificationService;
+    private final NotificationRepository notificationRepository;
     private final Logger logger = Logger.getLogger("com.grouproom.xyz.domain.friend.service.FriendManageServiceImpl");
 
     @Override
@@ -38,12 +44,23 @@ public class FriendManageServiceImpl implements FriendManageService {
         logger.info("modifyFriendDelete 호출");
 
         Friend friend = friendRepository.findByFromUserAndToUser(loginSeq, userSeq, true, false, false);
-        if(null == friend) {
+        if (null == friend) {
             logger.severe("친구 아님");
             throw new ErrorResponse(HttpStatus.BAD_REQUEST, "친구 아님");
         } else {
             friend.setIsDeleted(true);
         }
+
+        Notification notification = notificationRepository.findByUser_SequenceAndTargetSequenceAndIsDeletedAndNotificationType(loginSeq, userSeq, false, NotificationType.FRIEND);
+        if (null != notification) {
+            notificationService.removeNotification(loginSeq, notification.getSequence());
+        }
+
+        Notification reversedNotification = notificationRepository.findByUser_SequenceAndTargetSequenceAndIsDeletedAndNotificationType(userSeq, loginSeq, false, NotificationType.FRIEND);
+        if (null != reversedNotification) {
+            notificationService.removeNotification(loginSeq, reversedNotification.getSequence());
+        }
+
         return "";
     }
 
@@ -52,7 +69,7 @@ public class FriendManageServiceImpl implements FriendManageService {
 
         logger.info("findFriendByNickname 호출");
 
-        List<FriendUserResponse> friendUserResponseList = friendRepository.findNicknameByFromUserOrToUser(loginSeq, nickname, true,false,false);
+        List<FriendUserResponse> friendUserResponseList = friendRepository.findNicknameByFromUserOrToUser(loginSeq, nickname, true, false, false);
         return FriendListResponse.builder()
                 .friends(friendUserResponseList)
                 .build();
