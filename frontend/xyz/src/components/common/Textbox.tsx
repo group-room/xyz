c77c99360fee5beddc6d22ef3c6410d7f544ed34
송수현 <ssh1692@naver.com>
2023-05-24 09:54:33 +0900

    ✨ feat: 타임캡슐리팩토링 - 타임캡슐상세 주소 CSS 변경

--- frontend/xyz/src/components/common/Textbox.tsx
+++ frontend/xyz/src/components/common/Textbox.tsx
@@ -51,7 +51,7 @@ function Textbox({
         className={` ${
           maintextClass
             ? maintextClass
-            : "flex items-center justify-center px-1 h-full"
+            : "flex items-center justify-center px-1 h-auto"
         } `}
       >
         {maintext}
